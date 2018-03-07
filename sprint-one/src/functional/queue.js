4db4ede10a6c28d99ba823596d008fd5ca575e75
Monica Smith <monicasmith@Monicas-MacBook-Air-3.local>
2018-03-07 15:01:32 -0800

    First commit testing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,47 @@
+// var Queue = function() {
+//   var someInstance = {};
+//   var counter = 0;
+//   var currIndex = 0;
+//   var oldestIndex = 0;
+//
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//
+//   // Implement the methods below
+//
+//   someInstance.enqueue = function(value) {
+//     storage[currIndex] = value;
+//     console.log(JSON.stringify(storage));
+//     currIndex++;
+//     counter ++;
+//   };
+//
+//   someInstance.dequeue = function() {
+//     currIndex--;
+//     counter--;
+//     console.log("oldestIndex: " + oldestIndex);
+//     var keeper = storage[oldestIndex];
+//     console.log("keeper: " + keeper);
+//     delete storage[oldestIndex];
+//     oldestIndex++;
+//     console.log("DE:" + keeper);
+//     return keeper;
+//
+//
+//   };
+//
+//   someInstance.size = function() {
+//     return (counter < 0) ? 0 : counter;
+//   };
+//
+//   return someInstance;
+// };
+
 var Queue = function() {
   var someInstance = {};
+  var counter = 0;
+  var currIndex = 0;
+  var oldestIndex = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +49,28 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    (storage[currIndex]) ? storage[currIndex+1] = value : storage[currIndex] = value;
+    console.log(JSON.stringify(storage));
+    currIndex++;
+    counter ++;
   };
 
   someInstance.dequeue = function() {
+    currIndex--;
+    counter--;
+    console.log("oldestIndex: " + oldestIndex);
+    var keeper = storage[oldestIndex];
+    console.log("keeper: " + keeper);
+    delete storage[oldestIndex];
+    oldestIndex = counter;
+    console.log("DE:" + keeper);
+    return keeper;
+
+
   };
 
   someInstance.size = function() {
+    return (counter < 0) ? 0 : counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
+  var counter = 0;
+  var currIndex = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[currIndex] = value;
+    currIndex++;
+    counter++;
   };
 
   someInstance.pop = function() {
+    var keeper = storage[currIndex-1];
+    delete storage[currIndex-1];
+    currIndex--;
+    counter--;
+    return keeper;
   };
 
   someInstance.size = function() {
+    return ( counter < 0 ) ? 0 : counter;
   };
 
   return someInstance;
