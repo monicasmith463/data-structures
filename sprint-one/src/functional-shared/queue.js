537071632e3ea6083dc989ab3d603d5e9c96e7e7
Monica Smith <monicasmith@Monicas-MacBook-Air-3.local>
2018-03-07 17:21:02 -0800

    Second commit, currently on queue.js-functionalshared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,11 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.someInstance = {};
+  queue.counter = 0;
+  queue.currIndex = 0;
+  queue.oldestIndex = 0;
 };
 
 var queueMethods = {};
-
-
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.counter = 0;
+  stack.storage = {};
+  stack.currIndex = 0;
+  _.extend(stack, stackMethods);
+
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.currIndex] = value;
+  this.currIndex++;
+  this.counter++;
+};
 
+stackMethods.size = function() {
+  return ( this.counter < 0 ) ? 0 : this.counter;
+
+};
+
+stackMethods.pop = function() {
+  var keeper = this.storage[this.currIndex-1];
+  delete this.storage[this.currIndex-1];
+  this.currIndex--;
+  this.counter--;
+  return keeper;
+};
