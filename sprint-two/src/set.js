da3db63ea7ef6866d78114f7cd91d78e5a33fe20
monicasmith463 <monica.smith463@gmail.com>
2018-03-09 14:43:37 -0800

    finished Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,22 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
+  
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
