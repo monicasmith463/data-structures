c06176dcfe5850d739b7efe4d984a91e802fd8f4
monicasmith463 <monica.smith463@gmail.com>
2018-03-10 09:41:56 -0800

    finished tree and linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,15 +4,55 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.tail === null && list.head === null){
+      list.tail = Node(value);
+    } else if (list.tail && list.head === null){
+      list.head = list.tail;
+      list.head.next = value;
+      list.tail = Node(value);
+    } else {
+      var reference = list.head.next;
+    }
   };
 
   list.removeHead = function() {
+    // var head = Node();
+    if(list.head === null) {
+      return list.tail.value;
+    }
+    var rememberHead = list.head.value;
+    list.head.value = list.head.next;
+    return rememberHead;
   };
 
   list.contains = function(target) {
-  };
+    var found = false;
+    //debugger;
 
-  return list;
+    var inner = function(start){
+      if(list.head.value !== target){
+        if(list.head.next === null) {
+          return found;
+        } else if (list.head.next !== null && list.head.next === target){
+          found = true;
+          return found;
+        } else {
+          found = false;
+          return found;
+        }  
+      } else {
+        found = true;
+        return found;
+      }
+      inner(start.next);  
+    }
+    // return found;
+
+  found = inner(list.head);
+
+  return found;
+  };
+return list;
 };
 
 var Node = function(value) {
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,45 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  
+  
   // your code here
-  newTree.children = null;  // fix me
-
+  //children is an array. Add child should add a child array to the tree object
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
+  // console.log(JSON.stringify(newTree)); 
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.value = value;
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  debugger;
+  var found = false;
+
+  var recursiveFunc = function(start) {
+    // if(this.children) {
+      for(var i = 0; i < start.length; i ++){
+        if(start[i].value === target){
+          found = true;
+          return found;
+        } else {
+          found = false;
+          recursiveFunc(start[i]);
+        }
+      }
+    // }    
+      return found;
+
+  }
+  found = recursiveFunc(this.children);
+  return found;
 };
 
 
